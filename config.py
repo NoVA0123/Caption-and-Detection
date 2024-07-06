81ca6e53167e2906569ad795dcccc6cee185338e
Abhijit Rai <abhijit0123rai@gmail.com>
2024-07-06 10:49:46 +0530

    Added a config file

---
+++ config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+def get_config(MaxSeqLen: int):
+    return {
+            'batch_size': 8,
+            'num_epochs': 20,
+            'lr': 10**-4,
+            'seq_len': MaxSeqLen,
+            'd_model': 512,
+            'model_folder': 'model_files/weights/caption_model',
+            'model_basename': 'caption_model',
+            'preload': None,
+            'tokenizer_file': 'tokenizer.json',
+            'experiment_name': 'runs/tmodel'
+            }
+
+
+def get_weights_file_path(config, epoch: str):
+    ModelFolder = config['model_folder']
+    ModelBasename = config['model_basename']
+    ModelFilename = f'{ModelBasename}{epoch}.pt'
+    return str(Path('.') / ModelFolder / ModelFilename)
