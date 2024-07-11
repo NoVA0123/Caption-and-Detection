ec1e71434aad623c43da220ee7af6d9b56142788
Abhijit Rai <abhijit0123rai@gmail.com>
2024-07-11 19:22:53 +0530

    Modified positional encoding file

--- base_trainer/Transformer/positional_encoding.py
+++ base_trainer/Transformer/positional_encoding.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
+import math
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 class positionalencoding(nn.Module):
     '''https://arxiv.org/pdf/1706.03762'''
@@ -11,16 +13,25 @@ class positionalencoding(nn.Module):
         self.dModel = DModel
         self.seqLen = SeqLen
 
+        # Matrix of shape(seqlen, dmodel)
+        pe = torch.zeros(SeqLen, DModel)
+
+        # Create a vector of shape (seqlen)
+        Position = torch.arange(0, SeqLen, dtype=torch.float).unsqueeze(1)
+
+        # Create a vector of shape (dmodel)
+        DivTerm = torch.arange(0, DModel, 2).float() * (-math.log(10_000.0)/DModel)
+        DivTerm = torch.exp(DivTerm)
+
+        # Apply sine to even positions
+        pe[:, 0::2] = torch.sin(Position * DivTerm)
+        # Apply cosine to odd positions
+        pe[:, 1::2] = torch.cos(Position * DivTerm)
+        # Add batch dimention at 0th position
+        pe = pe.unsqueeze(0)
+        # Register the value
+        self.register_buffer('pe', pe)
 
     def forward(self, x):
-        EvenPos = torch.arange(0, self.dModel, 2).float()
-        Denom = torch.pow(10_000, EvenPos/self.dModel)
-
-        Positions = torch.arange(self.seqLen).reshape(self.seqLen, 1)
-        EvenPe = torch.sin(Positions/Denom)
-        OddPe = torch.sin(Positions/Denom)
-
-        stacking = torch.stack([EvenPe, OddPe], dim=2)
-        Pe = torch.flatten(stacking, start_dim=1, end_dim=2).to(device)
-        x = x + Pe.requires_grad_(False)
-        return x  
+        x = x + (self.pe[:, :x.shape[1], :]).to(device).requires_grad_(False)
+        return x
