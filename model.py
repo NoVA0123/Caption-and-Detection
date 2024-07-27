374f713752423d7313bd328c16c37416f088706e
Abhijit Rai <abhijit0123rai@gmail.com>
2024-07-27 11:08:30 +0530

    Added quality of life changes

--- model.py
+++ model.py
@@ -15,6 +15,8 @@ from base_files.dataset_files.json_extracter import caption_extracter
 from base_files.dataset_files.image_extracter import imgextracter
 
 
+# Configurig device
+print('Loading the device: \n\n')
 device = 'cpu'
 
 # Use GPU if it is available
@@ -24,6 +26,10 @@ if torch.cuda.is_available():
 # Use MPS if it is available(Apple devices only)
 elif hasattr(torch.backends, 'mps') and torch.backends.mps.is_available():
     device = 'mps'
+
+print('Device has been loaded!\n\n')
+print(f"Current Device: {device}")
+
 
 # Setting seed for reproducability
 torch.manual_seed(1337)
@@ -56,6 +62,7 @@ def train(JsonPath:str):
     null = None
     
     # Loading json
+    print("Loading the captions and image paths: \n\n")
     with open (JsonPath, 'r') as f:
         data = json.load(f)
 
@@ -65,21 +72,26 @@ def train(JsonPath:str):
     
     # Extracting caption and storing corresponding image path
     TrainData = caption_extracter(TrainJson, TrainImgPath)
+    print("Captions and image paths have been loaded into a dataframe. \n\n")
 
     # Creating a tokenizer
+    print('Creating tokenizer: \n\n')
     if data['tokenizer_config']['tokenizer_path'] is null:
         tokenizer = get_tokenizer(TrainData)
     else:
         tokenizer = get_tokenizer(TrainData,
                                   data['tokenizer_config']['tokenizer_path'])
 
+    print("Tokenizer has been created! \n\n")
+
     # Changing sample size
     TotalSamples = data['dataset_config']['max_sample']
-    TrainData = TrainData.sample(frac=TotalSamples,
+    TrainData = TrainData.sample(TotalSamples,
                                  random_state=1337).reset_index(drop=True)
     
     '''Initializing Config parameters'''
 
+    print('Initializing configuration: \n\n')
     # Initializing transformer config 
     TrConf = data['transformer_config']
     MaxLen = TrConf['block_size']
@@ -100,6 +112,7 @@ def train(JsonPath:str):
     Epochs = ModelConfig['epochs']
     
     # Downloading the Cnn model
+    print("Loading CNN model: \n\n")
     CnnConf = data['cnn_model_config']
     ExistingPath = CnnConf['existing_path']
     SpecificDownloadPath = CnnConf['specific_download_path']
@@ -111,9 +124,11 @@ def train(JsonPath:str):
     else:
         effnetv2s = get_cnn_model(MaxSeqLen=MaxLen,
                                   DModel=DModel)
+    print('Model has been loaded: \n\n')
 
 
     # Loading caption data into dataloader
+    print('Creating a DataLoader: \n\n')
     CaptionDataClass = texttoid(tokenizer=tokenizer,
                           MaxSeqLen=MaxLen,
                           dataframe=TrainData)
@@ -124,16 +139,21 @@ def train(JsonPath:str):
     ImgDataClass = imgextracter(dataframe=TrainData)
 
     ImgData = DataLoader(ImgDataClass, batch_size=BatchSize)
+    print("DataLoader for both Images and captions have been created.\n\n")
 
     # Initializing the transformer model
+    print("Initializing the model: \n\n")
     model = transformer(config=config,
                         CnnModel=effnetv2s)
     model.to(device) 
     # To compile model and make model faster
+    print("Model has been created. Now, Compiling the model: \n \n")
     model = torch.compile(model)
+    print("Model has been compiled. \n\n")
 
     '''Initializing optimizer'''
     # Making a decay learning rate
+    print("Configuring optimizer with decayed learning rate: \n\n")
     MaxLr = ModelConfig['learning_rate']['max_lr']
     MinLr = MaxLr * 0.1
     WarmupSteps = ModelConfig['learning_rate']['warmup_steps']
@@ -146,25 +166,56 @@ def train(JsonPath:str):
     optimizer = model.configure_optimizers(WeightDecay=0.1,
                                            LearningRate=6e-4,
                                            device=device)
+    print('Optimizer has been configured.')
+
+    # Creating gradient accumulation step to increase batch size
+    TotalBatchSize = 2**19
+    assert BatchSize % (BatchSize * MaxLen) == 0, "Make sure the total batch size is divisible by Batch * SeqLen"
+    GradAccumSteps = TotalBatchSize // (BatchSize * MaxLen)
+    print(f"Total batch size is: {TotalBatchSize} ")
+    print(f"-> calculated gradient accumulation steps: {GradAccumSteps}")
 
     # Training
     GlobalSteps = 0
     for i in tqdm(range(Epochs)):
-        LocalSteps = 0
-        for img, caption in zip(ImgData, CaptionData):
-            t0 = time.time() # Storing time of begining of the step
+        IterImgData = iter(ImgData)
+        IterCapData = iter(CaptionData)
 
+        LocalSteps = 0
+        for _ in range(len(ImgData)//GradAccumSteps):
+            t0 = time.time() # Storing time of begining of the step
             # Storing values
-            DecoderInput = caption['decoder_input'].to(device)
-            Label = caption['label'].to(device)
-            img = img.to(device)
 
             optimizer.zero_grad() # Setting optimizer to zero for every step
 
-            # Getting loss from the model
-            logits, loss = model(DecoderInput, img, Label)
+            # Accumulated gradient calculation
+            for _ in range(GradAccumSteps):
+                # Iterating the dataset
+                caption = next(IterCapData)
+                img = next(IterImgData)
+                
+                # Storing the values and converting them to device
+                DecoderInput = caption['decoder_input'].to(device)
+                Label = caption['label'].to(device)
+                img = img.to(device)
 
-            loss.backward() # Applying backpropogation
+                '''
+                Autocasting to datatypes of model to bfloat16 as it is 4x faster
+                than normal float32. It reduces the decimal value.
+                '''
+                
+                with torch.autocast(device_type=device,
+                                    dtype=torch.bfloat16):
+                    _, loss = model(DecoderInput, img, Label)
+
+                '''
+                To calculate Gradient accumulation for larger batches, we need
+                to add loss for each micro batch size and scaled it down during
+                each step.
+                '''
+                loss = loss / GradAccumSteps
+                loss.backward()
+
             # Applying norm on gradients to reduce shock of the model
             norm = torch.nn.utils.clip_grad_norm(model.parameters(), 1.0)
             
@@ -184,7 +235,7 @@ def train(JsonPath:str):
             dt = t1 - t0 
             TokensProcessed = BatchSize * MaxLen
             TokensPerSec = TokensProcessed/dt
-            print(f"Epoch: {i} | Steps: {LocalSteps} | loss: {loss.item(): .2f} | lr: {lr: .2f} | norm: {norm: .2f} | Process time: {dt*1000:.2f}ms | tok/sec: {TokensPerSec:.2f}")
+            print(f"Epoch: {i} | Steps: {LocalSteps} | loss: {loss.item(): .2f} | lr: {lr: .5e} | norm: {norm: .2f} | Process time: {dt*1000:.2f}ms | tok/sec: {TokensPerSec:.2f}")
 
             GlobalSteps += 1
             LocalSteps += 1
